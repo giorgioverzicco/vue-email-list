a5ac89946cf9b9f56db4af09bf860bb11b6bf42e
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-30 14:03:55 +0200

    add 10 mails by api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    emails: [],
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => this.emails.push(res.data.response));
+    }
+  },
 });
